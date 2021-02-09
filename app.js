fd4602d34b7da380ae1b1b0c7c96406339581b40
PG2S <geogutierrez15@outlook.com>
2021-02-09 15:55:42 -0500

    3

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 const botones = document.querySelector('#botones')
 const nombreUsuario = document.querySelector('#nombreUsuario')
+const contenidoProtegido = document.querySelector('#contenidoProtegido')
 
-firebase.auth().onAuthStateChanged(user => {
+firebase.auth().onAuthStateChanged( user => {
     if(user){
-        console.log(user);
+        console.log(user)
         botones.innerHTML = /*html*/`
-        <button class="btn btn-outline-danger mr-2">Cerrar Sesión</button>`
+        <button class="btn btn-outline-danger "id='btnCerrarSesion'>Cerrar Sesión</button>`
+        nombreUsuario.innerHTML = user.displayName
+        cerrarSesion()
+        contenidoProtegido.innerHTML = /*html*/`
+        <p class="text-center lead mt-5">Bienvenido ${user.email}</p>`
     }else{
         console.log('no existe user')
         botones.innerHTML = /*html*/` 
-        <button class="btn btn-outline-success mr-2">Acceder</button>`
+        <button class="btn btn-outline-success mr-2" id='btnAcceder'>Acceder</button>`
+
+        iniciarSesion()
+        nombreUsuario.innerHTML = 'Chat'
+        contenidoProtegido.innerHTML = /*html*/`
+        <p class="text-center lead mt-5">Debes iniciar sesión</p>`
     }
 })
+
+const cerrarSesion = () => {
+    const btnCerrarSesion = document.querySelector('#btnCerrarSesion')
+    btnCerrarSesion.addEventListener('click', () => {
+        firebase.auth().signOut()
+    })
+}
+
+const iniciarSesion = () => {
+    const btnAcceder = document.querySelector('#btnAcceder')
+    btnAcceder.addEventListener('click', async() => {
+        try {
+            const provider = new firebase.auth.GoogleAuthProvider()
+            await firebase.auth().signInWithPopup(provider)   
+        } catch (error) {
+            console.log(error);
+        }
+    })
+}
