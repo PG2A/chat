3c806b75f737b33afc1271c469f8cb7c8341de28
PG2S <geogutierrez15@outlook.com>
2021-02-09 15:05:21 -0500

    2

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 const botones = document.querySelector('#botones')
 const nombreUsuario = document.querySelector('#nombreUsuario')
+
+firebase.auth().onAuthStateChanged(user => {
+    if(user){
+        console.log(user);
+        botones.innerHTML = /*html*/`
+        <button class="btn btn-outline-danger mr-2">Cerrar Sesión</button>`
+    }else{
+        console.log('no existe user')
+        botones.innerHTML = /*html*/` 
+        <button class="btn btn-outline-success mr-2">Acceder</button>`
+    }
+})
